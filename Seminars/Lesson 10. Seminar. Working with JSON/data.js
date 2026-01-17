86cef0009bc764103bae6957e73f25757be4e0ed
yuriy_pidenko <yuriy.pidenko@gmail.com>
2026-01-17 11:46:38 +0300

    seminar 10

---
+++ Seminars/Lesson 10. Seminar. Working with JSON/data.js
@@ -0,0 +1,36 @@
+const data = `
+{
+        "summy": {
+                "src": "img.png",
+                "alt": "img obj data",
+                "name": "Название из obj data",
+                "link": "product.html",
+                "desc": "Описание товара из obj data",
+                "price": 21000
+        },
+        "jesse":{                
+                "src": "img2.png",
+                "alt": "img obj data new photo",
+                "name": "Название нового товара из obj data",
+                "link": "product2.html",
+                "desc": "Описание нового товара из obj data",
+                "price": 11000
+        },
+        "drew":{
+                "src": "img3.png",
+                "alt": "img obj data new photo 3",
+                "name": "Название нового товара из obj data 3",
+                "link": "product3.html",
+                "desc": "Описание нового товара из obj data 3",
+                "price": 33000
+        },
+        "jamie":{
+                "src": "img3.png",
+                "alt": "img obj data new photo 3",
+                "name": "Название нового товара из obj data 3",
+                "link": "product3.html",
+                "desc": "Описание нового товара из obj data 3",
+                "price": 33000
+        }
+
+}`
